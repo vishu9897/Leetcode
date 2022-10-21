01b422618962b879727f2c363d238ecf3923ad16
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2022-10-21 18:33:48 +0530

    Time: 221 ms (19.84%), Space: 10.5 MB (16.81%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(vector<int> &cuts,int i,int j,vector<vector<int>> &dp)
+    {
+        if(i==j) return 0;
+        int mini=INT_MAX;
+        if(dp[i][j]!=-1) return dp[i][j];
+        for(int k=i;k<j;k++)
+        {
+            int cases= cuts[j] - cuts[i-1] + solve(cuts,i,k,dp) + solve(cuts,k+1,j,dp); 
+            mini=min(mini,cases);
+        }
+        return dp[i][j]=mini;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        sort(cuts.begin(),cuts.end());
+        cuts.insert(cuts.begin(),0);
+        cuts.push_back(n);
+        vector<vector<int>> dp(cuts.size()+1, vector<int>(cuts.size()+1,-1));
+        return solve(cuts,1,cuts.size()-1,dp);
+    }
+};
