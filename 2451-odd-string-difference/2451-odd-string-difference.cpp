0099d54aa7b26e8910c5ac66bff8dac402bfe148
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2022-10-30 09:23:47 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (100.00%) - LeetHub

--- 2451-odd-string-difference/2451-odd-string-difference.cpp
+++ 2451-odd-string-difference/2451-odd-string-difference.cpp
@@ -7,8 +7,14 @@ public:
             m[ws[i][j + 1] - ws[i][j]].push_back(i);
         if (m.size() == 2)
         {
-            cout<<next(begin(m))->second.size()<<" "<<next(begin(m))->second.back()<<endl;
-            return ws[begin(m)->second.size() == 1 ? begin(m)->second.back() : next(begin(m))->second.back()];
+            for(auto x: m)
+            {
+                if(x.second.size()==1)
+                    return ws[x.second[0]];
+            }
+            // cout<<endl;
+            // cout<<next(begin(m))->second.size()<<" "<<next(begin(m))->second.back()<<endl;
+            // return ws[begin(m)->second.size() == 1 ? begin(m)->second.back() : next(begin(m))->second.back()];
         }
     }
     return ws[0];
