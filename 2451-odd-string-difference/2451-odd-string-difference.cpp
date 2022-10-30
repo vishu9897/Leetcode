e377421b44ff01dffd164aa4315b3ce089bdccfd
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2022-10-30 09:17:58 +0530

    Time: 3 ms (100.00%), Space: 7.4 MB (100.00%) - LeetHub

--- 2451-odd-string-difference/2451-odd-string-difference.cpp
+++ 2451-odd-string-difference/2451-odd-string-difference.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     string oddString(vector<string>& ws) {
     for (int j = 0; j + 1 < ws[0].size(); ++j) {
-        unordered_map<int, vector<int>> m;
+        map<int, vector<int>> m;
         for (int i = 0; i < ws.size(); ++i)
             m[ws[i][j + 1] - ws[i][j]].push_back(i);
         if (m.size() == 2)
+        {
+            cout<<next(begin(m))->second.size()<<" "<<next(begin(m))->second.back()<<endl;
             return ws[begin(m)->second.size() == 1 ? begin(m)->second.back() : next(begin(m))->second.back()];
+        }
     }
     return ws[0];
 }
