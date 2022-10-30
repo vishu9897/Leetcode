4f8d383f7fa029760eecb703d3f1acb91ba617a8
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2022-10-30 16:25:55 +0530

    Time: 325 ms (37.50%), Space: 76 MB (12.50%) - LeetHub

--- 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
+++ 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-   vector<int> secondGreaterElement(vector<int>& A) {
-    int n = A.size();
-    vector<vector<int>> mid(n, vector<int>(0));
-    stack<int> stk;
-    for (int i = 0; i < n; i++) {
-        while (stk.size() && A[stk.top()] < A[i]) {
-            mid[i].push_back(stk.top());
-            stk.pop();
+    vector<int> secondGreaterElement(vector<int>& nums) {
+        int n=nums.size();
+        stack<int> s1;
+        stack<int> s2;
+        stack<int> tmp;
+        vector<int> res(n,-1);
+        for(int i=0;i<n;i++)
+        {
+            while(!s2.empty() && nums[s2.top()] < nums[i])
+            {
+                res[s2.top()]=nums[i];
+                s2.pop();
+            }
+            while(!s1.empty() && nums[s1.top()] < nums[i])
+            {
+                tmp.push(s1.top());
+                s1.pop();
+            }
+            while(!tmp.empty())
+            {
+                s2.push(tmp.top());
+                tmp.pop();
+            }
+            s1.push(i);
         }
-        stk.push(i);
+        return res;
     }
-    priority_queue<vector<int>> pq;
-    vector<int> ans(n, -1);
-    for (int i = 0; i < n; i++) {
-        while(pq.size() && -pq.top()[0] < A[i]) {
-            ans[pq.top()[1]] = A[i];
-            pq.pop();
-        }
-        for (int& j: mid[i])
-            pq.push({-A[j], j});
-    }
-    return ans;
-}
 };
