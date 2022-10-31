58b58e85c5d8e19d23acd1d634b5310f5e5e2864
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2022-10-31 15:13:16 +0530

    Time: 177 ms (39.93%), Space: 47.2 MB (16.56%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
-    int f(int x, int K, int N) {
-        int ans = 0, r = 1;
-        for (int i = 1; i <= K; ++i) {
-            r *= x-i+1;
-            r /= i;
-            ans += r;
-            if (ans >= N) break;
+     int superEggDrop(int K, int N) {
+        vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
+        int m = 0;
+        while (dp[m][K] < N) {
+            m++;
+            for (int k = 1; k <= K; ++k)
+                dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
         }
-        return ans;
-    }
-    int superEggDrop(int K, int N) {
-         int lo = 1, hi = N;
-        while (lo < hi) {
-            int mi = (lo + hi) / 2;
-            if (f(mi, K, N) < N)
-                lo = mi + 1;
-            else
-                hi = mi;
-        }
-
-        return lo;
+        return m;
     }
 };
