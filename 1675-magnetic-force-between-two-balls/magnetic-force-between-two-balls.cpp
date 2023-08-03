4323e2dca914753ae1237fd916d239cf81d774e9
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2023-08-03 12:57:21 +0530

    Time: 119 ms (99.70%) | Memory: 57.8 MB (90.16%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -4,20 +4,16 @@ public:
         sort(position.begin(),position.end());
         int ans=INT_MIN,n=position.size();
         int low=1,high=(position[n-1]-position[0])/(m-1);
-        // cout<<high<<endl<<endl;
         while(low<=high)
         {
             int mid= low + (high-low)/2,temp=0,magnet=1;
-            // cout<<low<<" "<<mid<<" "<<high<<endl;
             for(int i=1;i<n;i++){
                 if(position[i]-position[temp] >= mid)
                 {
-                    // cout<<position[temp]<<" "<<position[i]<<endl;
                     magnet++;
                     temp=i;
                 }
             }
-            // cout<<magnet<<endl;
             if(magnet>=m)
             {
                 ans=max(ans,mid);
