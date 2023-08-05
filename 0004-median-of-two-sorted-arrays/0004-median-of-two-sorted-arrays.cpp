5a9fb586248b4018705736926f7689741fac82a8
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2023-08-05 13:03:24 +0530

    Time: 16 ms (99.05%), Space: 89.2 MB (73.48%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // We Want to do binary search on smaller array that's why
-        if(nums2.size() < nums1.size()){
-            return findMedianSortedArrays(nums2,nums1);
+        if(nums1.size() > nums2.size()) return findMedianSortedArrays(nums2,nums1);
+        int n1=nums1.size(),n2=nums2.size(),low=0,high=n1,even= !((n1+n2) & 1);
+        while(low<=high)
+        {
+            int mid1= low + (high - low)/2;
+            int mid2= (n1+n2+1)/2 -mid1;
+
+            int l1= mid1-1 == -1 ? INT_MIN :  nums1[mid1-1];
+            int l2= mid2-1 == -1 ? INT_MIN : nums2[mid2-1];
+            int r1= mid1 == n1 ? INT_MAX : nums1[mid1];
+            int r2= mid2 == n2 ? INT_MAX : nums2[mid2];
+
+            if(l1<=r2 && l2<=r1)
+            {
+                if(even) return (double)(max(l1,l2) + min(r1,r2))/2.0;
+                else return max(l1,l2);
+            }
+            if(l1>r2) high=mid1-1;
+            else low=mid1+1;
         }
-
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        // Doing binary search in nums1
-        int s = 0, e = n1;
-
-        while(s <= e){
-            // Making the partision
-            int cut1 = s + (e - s) / 2;
-            // Choosing remaining element from nums2
-            int cut2 = (n1 + n2 + 1) / 2 - cut1;
-
-            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
-            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
-
-            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
-            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
-
-            if(left1 <= right2 && left2 <= right1){
-                // if odd length
-                if((n1 + n2) & 1){
-                    return max(left1,left2);
-                }
-                // if even length
-                return (max(left1,left2) + min(right1,right2)) / 2.0;
-            }
-            else if(left1 > right2){
-                e = cut1 - 1;
-            }
-            else{
-                s = cut1 + 1;
-            }
-        }
-
-        return 0.0;
+        return 0;
     }
 };
