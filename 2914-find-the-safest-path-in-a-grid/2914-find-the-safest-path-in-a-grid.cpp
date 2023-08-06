d2addd0027841f2310224188d2f11461a86887d2
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2023-08-06 19:16:41 +0530

    Time: 2654 ms (7.14%), Space: 112.1 MB (7.14%) - LeetHub

--- 2914-find-the-safest-path-in-a-grid/2914-find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/2914-find-the-safest-path-in-a-grid.cpp
@@ -7,33 +7,28 @@ public:
         int maxi=INT_MIN;
         vis[i][j]=1;
         if(i==n-1 && j==m-1) return true;
-        // cout<<i<<" -- "<<j<<endl;
         bool ans1=false,ans2=false,ans3=false,ans4=false;
         if(i+1 < n && vis[i+1][j]==0 && dist[i+1][j]>=mid)
         {
-            // cout<<"1 condition"<<endl;
             ans1= solve(dist,vis,mid,i+1,j);
         }
         if(j+1 < m && vis[i][j+1]==0 && dist[i][j+1]>=mid)
         { 
-            // cout<<"2 condition"<<endl;
             ans2= solve(dist,vis,mid,i,j+1);
         }
         if(i-1 >= 0 && vis[i-1][j]==0 && dist[i-1][j]>=mid)
         {
-            // cout<<"3 condition"<<endl;
             ans3= solve(dist,vis,mid,i-1,j);
         }
         if(j-1 >=0  && vis[i][j-1]==0 && dist[i][j-1]>=mid)
         {
-            // cout<<"4 condition"<<endl;
             ans4= solve(dist,vis,mid,i,j-1);
         }
         return  ans1 || ans2 || ans3 || ans4;
     }
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         vector<pair<int,int>> pr;
-        vector<vector<int>> dist(grid.size(),vector<int>(grid[0].size(),-1)),vis(grid.size(),vector<int>(grid[0].size(),0));
+        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),0));
         int low=0,high=0,ans=0;
         for(int i=0;i<grid.size();i++)
         {
@@ -52,30 +47,20 @@ public:
                     {
                         mini=min(mini,abs(x.first-i) + abs(x.second-j));
                     }
-                    dist[i][j]=mini;
+                    grid[i][j]=mini;
                     high=max(high,mini);
                 }
                 else
                 {
-                    dist[i][j]=INT_MIN;
+                    grid[i][j]=INT_MIN;
                 }
             }
         }
-        // for(int i=0;i<grid.size();i++)
-        // {
-        //     for(int j=0;j<grid[0].size();j++)
-        //     {
-        //         cout<<dist[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        // cout<<high<<endl;
         while(low<=high)
         {
             int mid= low + (high-low)/2;
-            if(solve(dist,vis,mid,0,0))
+            if(solve(grid,vis,mid,0,0))
             {
-                // cout<<"yes"<<" "<<mid<<endl;
                 ans=max(ans,mid);
                 low=mid+1;
                
@@ -91,9 +76,7 @@ public:
                     vis[i][j]=0;
                 }
             }
-            // cout<<"NO "<<mid<<endl;
         }
-        // cout<<low<<" "<<high<<endl;
         return high==-1 ? 0 : high;
         
     }
