ed679a160404dd59f2d85d5804b54b96b984601c
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2023-08-13 10:04:59 +0530

    Time: 223 ms (100.00%) | Memory: 118.1 MB (20.00%) - LeetSync

--- 3000-minimum-absolute-difference-between-elements-with-constraint/minimum-absolute-difference-between-elements-with-constraint.cpp
+++ 3000-minimum-absolute-difference-between-elements-with-constraint/minimum-absolute-difference-between-elements-with-constraint.cpp
@@ -5,10 +5,24 @@ public:
         multiset<int> s;
         for(int i = x; i < nums.size(); ++i){
             s.insert(nums[i-x]);
-            auto it = s.upper_bound(nums[i]);
-            if(it != s.end()) ans = min(ans, abs(nums[i] - *it));
-            if(it != s.begin()) ans = min(ans, abs(nums[i] - *prev(it)));
+            auto it = s.lower_bound(nums[i]);
+            int ans1;
+            // if(i==4) cout<<"it "<<*it<<endl;
+            if(it != s.end())
+            {
+                // cout<<"first ";
+                ans = min(ans, abs(nums[i] - *it));
+                ans1=*it;
+            } 
+            if(it!=s.begin())
+            {
+                ans = min(ans, abs(nums[i] - *prev(it)));
+                // cout<<"second ";
+                ans1=*prev(it);
+            } 
+            // cout<<i<<" "<<nums[i]<<" "<<ans1<<endl;
         }
+        // for(auto x:s) cout<<x<<" ";
         return ans;
     }
 };
