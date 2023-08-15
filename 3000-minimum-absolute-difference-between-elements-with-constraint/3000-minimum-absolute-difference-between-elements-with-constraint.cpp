a4c6cf738d51f7daf5eb1559693de6a90dcc4fd9
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2023-08-15 20:57:29 +0530

    Time: 220 ms (70.12%), Space: 118 MB (89.26%) - LeetHub

--- 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.cpp
+++ 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.cpp
@@ -5,9 +5,18 @@ public:
         multiset<int> s;
         for(int i = x; i < nums.size(); ++i){
             s.insert(nums[i-x]);
-            auto it = s.upper_bound(nums[i]);
-            if(it != s.end()) ans = min(ans, abs(nums[i] - *it));
-            if(it != s.begin()) ans = min(ans, abs(nums[i] - *prev(it)));
+            auto it = s.lower_bound(nums[i]);
+            int ans1;
+            if(it != s.end())
+            {
+                ans = min(ans, abs(nums[i] - *it));
+                ans1=*it;
+            } 
+            if(it!=s.begin())
+            {
+                ans = min(ans, abs(nums[i] - *prev(it)));
+                ans1=*prev(it);
+            }
         }
         return ans;
     }
