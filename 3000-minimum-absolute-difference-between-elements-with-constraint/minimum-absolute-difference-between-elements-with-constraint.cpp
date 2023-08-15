b26111fc283606a5ded25fcf273f39f447e7920c
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2023-08-15 20:57:30 +0530

    Time: 220 ms (70.12%) | Memory: 118 MB (89.26%) - LeetSync

--- 3000-minimum-absolute-difference-between-elements-with-constraint/minimum-absolute-difference-between-elements-with-constraint.cpp
+++ 3000-minimum-absolute-difference-between-elements-with-constraint/minimum-absolute-difference-between-elements-with-constraint.cpp
@@ -7,22 +7,17 @@ public:
             s.insert(nums[i-x]);
             auto it = s.lower_bound(nums[i]);
             int ans1;
-            // if(i==4) cout<<"it "<<*it<<endl;
             if(it != s.end())
             {
-                // cout<<"first ";
                 ans = min(ans, abs(nums[i] - *it));
                 ans1=*it;
             } 
             if(it!=s.begin())
             {
                 ans = min(ans, abs(nums[i] - *prev(it)));
-                // cout<<"second ";
                 ans1=*prev(it);
-            } 
-            // cout<<i<<" "<<nums[i]<<" "<<ans1<<endl;
+            }
         }
-        // for(auto x:s) cout<<x<<" ";
         return ans;
     }
 };
