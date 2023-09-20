3885f1d0f69dc420e05384528c7bcff6ca01adb3
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2023-09-20 07:22:58 +0530

    Time: 7 ms (54.3%), Space: 13.9 MB (8.91%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lower=-1,upper=-1,low=0,high=nums.size()-1;
-
+        int low=0,high=nums.size()-1;
+        int mini=INT_MAX,maxi=-1;
         while(low <= high)
         {
-            int mid= low + (high - low)/2;
-            if(nums[mid] == target){
-                lower=mid;
-                high=mid-1;
-            }
-            else if(nums[mid] < target)
-            {
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
-            }
-        }
-        low=0,high=nums.size()-1;
-        while(low<=high)
-        {
-            int mid= low + (high - low)/2;
+            int mid= low + (high-low)/2;
             if(nums[mid]==target)
             {
-                upper=mid;
-                low=mid+1;
+                mini=min(mini,mid);
+                high=mid-1;    
             }
-            else if(nums[mid] < target)
-            {
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
-            }
+            else if(nums[mid]>target) high=mid-1;
+            else low=mid+1;
         }
-        return {lower,upper}; 
+        low=0,high=nums.size()-1;
+        while(low <= high)
+        {
+            int mid= low + (high-low)/2;
+            if(nums[mid]==target)
+            {
+                maxi=max(maxi,mid);
+                low=mid+1;    
+            }
+            else if(nums[mid]>target) high=mid-1;
+            else low=mid+1;
+        } 
+        if(mini==INT_MAX) mini=-1;
+        return {mini,maxi};   
     }
 };
