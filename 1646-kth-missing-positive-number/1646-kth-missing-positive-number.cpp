dadfb9ddf1c253de52d0aeee7629635ffd199155
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2023-09-21 19:56:26 +0530

    Time: 0 ms (100%), Space: 9.8 MB (15.96%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -4,12 +4,11 @@ public:
         int low=0,high=arr.size()-1;
         while(low<=high)
         {
-            int mid= (low + high)/2;
-            int miss= arr[mid]- (mid +1);
+            int mid= low + (high-low)/2;
+            int miss= arr[mid] - (mid+1);
             if(miss<k) low=mid+1;
             else high=mid-1;
-        }   
-        // return 0;
-        return low+k;
+        }
+        return k+low;
     }
 };
