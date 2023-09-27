853aa7d1bbe36b7ce085207236a7b14162d0296e
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2023-09-27 10:08:45 +0530

    Time: 35 ms (19.37%), Space: 89.6 MB (57.94%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         if(nums1.size() > nums2.size()) return findMedianSortedArrays(nums2,nums1);
-        int n1=nums1.size(),n2=nums2.size(),low=0,high=n1,even= !((n1+n2) & 1);
+        int n1=nums1.size(),n2=nums2.size(),low=0,high=n1,even =  !((n1+n2) & 1);
         while(low<=high)
         {
-            int mid1= low + (high - low)/2;
-            int mid2= (n1+n2+1)/2 -mid1;
-
-            int l1= mid1-1 == -1 ? INT_MIN :  nums1[mid1-1];
+            int mid1= low + (high-low)/2;
+            int mid2=  (n1+n2+1)/2 -mid1;
+            int l1= mid1-1 == -1 ? INT_MIN : nums1[mid1-1];
             int l2= mid2-1 == -1 ? INT_MIN : nums2[mid2-1];
             int r1= mid1 == n1 ? INT_MAX : nums1[mid1];
             int r2= mid2 == n2 ? INT_MAX : nums2[mid2];
-
             if(l1<=r2 && l2<=r1)
             {
-                if(even) return (double)(max(l1,l2) + min(r1,r2))/2.0;
-                else return max(l1,l2);
+                if(even) return (double) (max(l1,l2) + min(r1,r2))/2.0;
+                return max(l1,l2);
             }
             if(l1>r2) high=mid1-1;
             else low=mid1+1;
