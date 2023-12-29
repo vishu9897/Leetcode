a131da186a67ea683040387dafa81a03167a1f17
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2023-12-29 12:06:06 +0530

    Time: 169 ms (10.32%) | Memory: 19 MB (36.28%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -3,19 +3,22 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         int ans;
-        vector<vector<int>> dp(n+3,vector<int>(amount+2,INT_MAX));
-        for(int i=0;i<n;i++) dp[i][0]=0;
-        for(int i=0;i<=amount;i++) if(i%coins[0]==0) dp[0][i]=i/coins[0];
+        vector<int> curr(amount+2,INT_MAX), prev(amount+2,INT_MAX);
+        // vector<vector<int>> dp(n+3,vector<int>(amount+2,INT_MAX));
+        for(int i=0;i<n;i++) prev[0]=0;
+        for(int i=0;i<=amount;i++) if(i%coins[0]==0) prev[i]=i/coins[0];
+        curr=prev;
         for(int i=1;i<n;i++)
         {
             for(int j=0;j<=amount;j++)
             {
-                long long take=INT_MAX,ntake= dp[i-1][j];
-                if(j>=coins[i]) take=(long long) 1+ dp[i][j-coins[i]];
-                dp[i][j]=min(take,ntake);
+                long long take=INT_MAX,ntake= prev[j];
+                if(j>=coins[i]) take=(long long) 1+ curr[j-coins[i]];
+                curr[j]=min(take,ntake);
             }
+            prev=curr;
         }
-        ans=dp[n-1][amount];
+        ans=curr[amount];
         return ans>=INT_MAX ? -1 : ans;
     }
 };
