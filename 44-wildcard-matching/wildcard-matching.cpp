fe621a9c515aa5473b8f37078c8382eb6b5f6366
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-01-02 10:29:56 +0530

    Time: 351 ms (7.64%) | Memory: 11.8 MB (58.82%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool solve(string &s,string &p,int index1,int index2,vector<vector<int>> &dp)
-    {
-        if(index1<0 && index2<0) return true;
-        if(index2<0 && index1>=0) return false;
-        if(index2>=0 && index1<0){
-            for(int i=index2;i>=0;i--){
-                if(p[i]!='*') return false;
-            }
-            return true;
-        }
-        if(dp[index1][index2]!=-1) return dp[index1][index2];
-        if(s[index1]==p[index2] || p[index2]=='?') return dp[index1][index2] = solve(s,p,index1-1,index2-1,dp);
-        if(p[index2]=='*') return dp[index1][index2] = solve(s,p,index1,index2-1,dp) | solve(s,p,index1-1,index2,dp);
-        return dp[index1][index2] = false;
-    }
+    
     bool isMatch(string s, string p) {
         int n1=s.size(),n2=p.size();
-        vector<vector<int>> dp(n1+2,vector<int>(n2+4,-1));
-        return solve(s,p,n1-1,n2-1,dp);
+        vector<vector<bool>> dp(n1+2,vector<bool>(n2+4,false));
+        for(int i=1;i<=n2;i++) {
+            if(p[i-1]!='*') break;
+            dp[0][i]=true;
+        }
+        dp[0][0]=true;
+        for(int i=1;i<=n1;i++)
+        {
+            for(int j=1;j<=n2;j++)
+            {
+                if(s[i-1]==p[j-1] || p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
+                else if(p[j-1]=='*') dp[i][j]= dp[i-1][j] | dp[i][j-1];
+                else dp[i][j]=false;
+            }
+        }
+        return dp[n1][n2];
     }
 };
