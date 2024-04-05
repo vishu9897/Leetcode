fdf93919b9780ac84114f2d7c53791b4d668b2f6
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-04-05 12:35:38 +0530

    Time: 4 ms (41.50%) | Memory: 13.6 MB (5.84%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1,mid;
-        while(low<=high)
+        int low=0,high=nums.size()-1,ans=-1;
+        while(low <= high)
         {
-            mid=low + (high-low)/2;
-            cout<<nums[low]<<" "<<nums[mid]<<" "<<nums[high]<<endl;
-            if(nums[mid] == target) return mid;
-            if(nums[low] <= nums[mid] && nums[mid]<=nums[high])
-            {
-                if(target<nums[mid]) high=mid-1;
-                else low=mid+1;
-            }
-            else if(nums[low]<=target && target<=nums[mid]) high=mid-1;
-            else if(nums[mid]<=target && target<= nums[high]) low=mid+1;
-            else if(nums[low] > nums[mid]) 
-            {
-                high=mid-1;
-            }
-            else low=mid+1;
-        }
-        return -1;
+            
+            int mid= low + (high-low)/2;
+            if(nums[mid]==target) ans=mid;
+            if(nums[low] > nums[mid]){
+                if(nums[mid]<=target && target<=nums[high]) low=mid+1;
+                else high=mid-1;
+            }   
+            else if(nums[mid] > nums[high]){
+                if(nums[low] <= target && target <= nums[mid]) high=mid-1;
+                else low= mid+1;
+            } 
+            else if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
+        } 
+        return ans;
     }
 };
