7d89b928080d01acc73c17d28763593b47cacf97
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-04-08 11:25:02 +0530

    Time: 15 ms (23.97%) | Memory: 14.1 MB (29.44%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    void DFS(vector<vector<char>>&board,vector<vector<int>>& visited,int m,int n,int rw,int cl,int drow[],int dcol[],vector<vector<char>> &ans)
+    void DFS(vector<vector<char>>&board,vector<vector<int>>& visited,int m,int n,int rw,int cl,int drow[],int dcol[])
     {
-        ans[rw][cl]='O';
         visited[rw][cl]=1;
 
         for(int i=0;i<4;i++)
         {
             int row= rw + drow[i];
             int col= cl + dcol[i];   
-            if(row>=0 && col>=0 && row<n && col<m && board[row][col]=='O' && visited[row][col]!=1)
+            if(row>=0 && col>=0 && row<n && col<m && board[row][col]=='O' && visited[row][col]==0)
             {
-                DFS(board,visited,m,n,row,col,drow,dcol,ans);
+                DFS(board,visited,m,n,row,col,drow,dcol);
             }
         }
         
@@ -21,20 +20,25 @@ public:
         int n=board.size();
         int m=board[0].size();
         vector<vector<int>> visited(n,vector<int>(m,0));
-        vector<vector<char>> ans(n,vector<char>(m,'X'));
         int drow[]= {-1,0,0,1};
         int dcol[]= {0,-1,1,0};
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++)
             {
-                if(visited[i][j]==0 && (i==0 || j==0 || i==n-1 || j==m-1) && board[i][j]=='O')
+                if((i==0 || j==0 || i==n-1 || j==m-1) && board[i][j]=='O' && visited[i][j]==0)
                 {
-                    DFS(board,visited,m,n,i,j,drow,dcol,ans);
+                    DFS(board,visited,m,n,i,j,drow,dcol);
                 }
 
             }
         }
-        board= ans;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                board[i][j]= visited[i][j]==1 ? 'O' : 'X';
+            }
+        }
     }
 };
