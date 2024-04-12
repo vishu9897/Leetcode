6f99062be41a4334672deb3b09f44f98ed325548
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-04-12 18:37:25 +0530

    Time: 16 ms (10.35%) | Memory: 16.3 MB (20.86%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int checkRow=0,checkCol=0;
+        for(int i=0;i<matrix.size();i++)
+        {
+            if(matrix[i][0]==0) checkRow=1; 
+        }
+        for(int j=0;j<matrix[0].size();j++)
+        {
+            if(matrix[0][j]==0) checkCol=1; 
+        }
+        for(int i=1;i<matrix.size();i++)
+        {
+            for(int j=1;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<matrix.size();i++)
+        {
+            if(matrix[i][0]==0)
+            {
+            for(int j=1;j<matrix[0].size();j++)
+            {
+                matrix[i][j]=0;
+            }}
+        }
+        for(int i=1;i<matrix[0].size();i++)
+        {
+            if(matrix[0][i]==0){
+
+            
+            for(int j=1;j<matrix.size();j++)
+            {
+                matrix[j][i]=0;
+            }
+            }
+        }
+        if(checkRow){
+            for(int i=0;i<matrix.size();i++)
+                matrix[i][0]=0;
+        }
+        if(checkCol)
+        {
+            for(int i=0;i<matrix[0].size();i++)
+                matrix[0][i]=0;
+        }
+    }
+};
