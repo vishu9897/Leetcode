35abe2f5f8b4197d70fd8edfa4846b4bf67a5f81
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-04-27 22:49:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (64.24%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int> &nums,int index,int n,vector<vector<int>> &dp,int ta){
+    int solve(vector<int> &nums,int index,int n,vector<int> &dp,int ta){
         if(index>=n) return 0;
-        if(dp[index][ta]!=-1) return dp[index][ta];
+        if(dp[index]!=-1) return dp[index];
         int take=INT_MIN,ntake=INT_MIN;
-        // cout<<index<<" "<<nums[index]<<" "<<n<<endl;
         take= nums[index]+ solve(nums,index+2,n,dp,1);
         ntake= solve(nums,index+1,n,dp,0);
-        return dp[index][ta]=max(take,ntake);
+        return dp[index]=max(take,ntake);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==0) return 0;
         if(n==1) return nums[0];
-        vector<vector<int>> dp(n+4,vector<int>(2,-1));
-        vector<vector<int>> dp1(n+4,vector<int>(2,-1));
-        // return solve(nums,0,n-1,dp);
+        vector<int> dp(n+4,-1);
+        vector<int> dp1(n+4,-1);
         return max(solve(nums,0,n-1,dp,1),solve(nums,1,n,dp1,1));
     }
 };
