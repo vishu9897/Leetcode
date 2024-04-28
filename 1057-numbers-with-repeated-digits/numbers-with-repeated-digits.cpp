1d0d270578cb1b69322642bb4e3e5dc04f504843
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-04-28 20:32:02 +0530

    Time: 12 ms (36.29%) | Memory: 7.3 MB (79.32%) - LeetSync

--- 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.cpp
+++ 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
-    int dp[10][2][2][2][1<<10];
-vector<int>num;
-int solve(int pos,int tight,int start,int rep,int mask)
-{
-    if(pos == num.size())
-    {
-        return rep;
-    }
-    int &ans= dp[pos][tight][start][rep][mask];
-    if(ans!=-1)return ans;
-    
-    int k = num[pos];
-    if(tight)k=9;
-    int res=0;
-    for(int i=0;i<=k;i++)
-    {
-        int ns = start||i>0;//number started yet or not
-        int nt = tight||i<k;//tight for next number
-        if(ns){
-            res+=solve(pos+1,nt,ns,rep||(mask&(1<<i)),mask|1<<i);
-        }
-        else{
-            res+=solve(pos+1,nt,0,rep,mask);
-        }
-    
-    }
-    ans= res;
-    return res;
-}
-int numDupDigitsAtMostN(int N) {
-    while(N){
-        num.push_back(N%10);
-        N/=10;
-    }
-    reverse(num.begin(),num.end());
-    memset(dp,-1,sizeof(dp));
-    return solve(0,0,0,0,0);
-}
+
+	int dp[11][2][2][(1 << 10)];
+
+
+	int solve(int pos, bool bound, bool hasRepeated, int mask, string &s) {
+
+		if (pos == s.length()) {
+			return hasRepeated ? 1 : 0;
+		}
+
+		if (dp[pos][bound][hasRepeated][mask] != -1) return dp[pos][bound][hasRepeated][mask];
+
+		int maxDigit = bound ? s[pos] - '0' : 9;
+        int ans=0;
+
+		for (int digit = 0; digit <= maxDigit; digit++) {
+
+			/* if this is a leading zero, then hasRepeated for the the current guy (position) will obviously be false  */
+			if (digit == 0 && mask == 0) ans += solve(pos + 1, bound && (digit == s[pos] - '0'), false, mask, s);
+
+			/*
+				if this particular has already come earlier in this digit, and it is not a leading zero, then clearly, there is a repitition
+				and we have to pass true in hasRepeated
+			*/
+			else if ((mask & (1 << digit))) ans += solve(pos + 1, bound && (digit == s[pos] - '0'), true, mask, s);
+
+			/*
+				if this guy is coming for the first time, then hasRepeated will be whatever was for the previous guy
+				and don't forget to switch on the bit corresponding to this digit in the mask
+			*/
+			else ans += solve(pos + 1, bound && (digit == s[pos] - '0'), hasRepeated, (mask | (1 << digit)), s);
+
+		}
+
+		return dp[pos][bound][hasRepeated][mask] = ans;
+
+	}
+
+	int numDupDigitsAtMostN(int n) {
+		memset(dp, -1, sizeof(dp));
+		string s = to_string(n);
+		// return solve(0, true, false, false, 0, R);
+		return solve(0, true, false, 0, s);
+	}
 };
