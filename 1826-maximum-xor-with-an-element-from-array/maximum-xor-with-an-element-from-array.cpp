d3b702f8e198c64448670df70b047a14df787025
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-05-02 10:25:47 +0530

    Time: 566 ms (42.01%) | Memory: 220.2 MB (84.32%) - LeetSync

--- 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.cpp
+++ 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.cpp
@@ -1,69 +1,82 @@
-struct Node{
-    Node* arr[2];
-
-    bool containsKey(int bit)
-    {
-        return arr[bit]!=NULL;
-    }
-    void putKey(int bit,Node* node)
-    {
-        arr[bit]=node;
-    }
-    Node* getKey(int bit)
-    {
-        return arr[bit];
-    }
-};
 class Solution {
+private:
+	struct Trie{
+		Trie* links[2];
+		bool containsKey(int i)
+		{	
+			return links[i]!=NULL;
+		}
+        void putKey(int bit,Trie* node)
+        {
+            links[bit]=node;
+        }
+        Trie* getKey(int bit)
+        {
+            return links[bit];
+        }
+};
 public:
     vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=queries.size();
         sort(nums.begin(),nums.end());
-        vector<pair<int,pair<int,int>>> oQ;
-        int n=queries.size(),index=0,size=nums.size();
-        vector<int>ans (n,-1);
-        Node* root= new Node();
-        for(int i=0;i<n;i++) oQ.push_back({queries[i][1],{queries[i][0],i}});
+      	vector<int> ans(n,-1);
+	    vector<pair<int,pair<int,int>>> oQ;
+        Trie* root= new Trie();
+        int index=0;
+        for(int i=0;i<queries.size();i++)
+        {
+            int query1= queries[i][0];
+            int query2=queries[i][1];
+            int index=i;
+            oQ.push_back({query2,{query1,index}});	
+        }
+
+
         sort(oQ.begin(),oQ.end());
 
-        for(int i=0;i<n;i++)
-        {
-            int ai=oQ[i].first;
-            int Xele=oQ[i].second.first;
-            int ind=oQ[i].second.second;
 
-            while(index<size && nums[index]<=ai)
+        for(int i=0;i<oQ.size();i++){
+            int ele= oQ[i].second.first;
+            int range= oQ[i].first;
+            int in= oQ[i].second.second;
+
+
+            while(index<nums.size() && nums[index] <= range)
             {
-                Node* node=root;
-                for(int j=31;j>=0;j--)
+                Trie* node=root;
+                int value=nums[index];
+                for(int i=31;i>=0;i--)
                 {
-                    int num= (nums[index]>>j) & 1;
-                    if(!node->containsKey(num))
+                    int val= (value>>i) & 1;
+                    if(!node->containsKey(val))
                     {
-                        node->putKey(num,new Node());
+                        node->putKey(val,new Trie());
                     }
-                    node=node->getKey(num);
+                    node=node->getKey(val);
                 }
                 index++;
             }
-            if(index!=0)
-            {
-                int temp=0;
-                Node* node=root;
 
-                for(int j=31;j>=0;j--)
+
+            if(index!=0){
+                int temp=0;
+                Trie* node=root;
+                for(int i=31;i>=0;i--)
                 {
-                    int num= (Xele >> j) & 1;
-                    if(node->containsKey(1-num))
+                    int val= (ele>>i) & 1;
+                    if(node->containsKey(1-val))
                     {
-                        temp=temp| (1<<j);
-                        node=node->getKey(1-num);
+                        temp=temp|(1<<i);
+                        node=node->getKey(1-val);
                     }
                     else
-                        node=node->getKey(num);
+                        node=node->getKey(val);
                 }
-                ans[ind]=temp;
+                ans[in]=temp;
             }
         }
-        return ans;
-    }
+        return ans;   
+		
+	}
 };
+
