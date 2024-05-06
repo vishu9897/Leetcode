478e5ecc8b0dd1068349bd7e98c0822735732569
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-05-06 12:07:58 +0530

    Time: 8 ms (60.06%) | Memory: 20 MB (32.84%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,31 +1,16 @@
 class Solution {
-private:
-    int solve(vector<int>& coins,int amount,int index,vector<vector<int>>& dp){
-        if(index<0) return amount==0 ? 1 : 0;
-        if(dp[amount][index]!=-1) return dp[amount][index];
-        int take=0,ntake=0;
-        if(amount-coins[index]>=0)  take= solve(coins,amount-coins[index],index,dp);   
-        ntake= solve(coins,amount,index-1,dp);
-        return dp[amount][index]=take+ntake;
-    }
-
 public:
+    int solve(int amount,int index,vector<int>&coins,vector<vector<int>> &dp)
+    {
+        if(index<0) return amount==0 ? 1 : 0;
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        int take=0,ntake= solve(amount,index-1,coins,dp);
+        if(coins[index] <= amount) take= solve(amount-coins[index],index,coins,dp);
+        return dp[index][amount] = take+ntake;
+    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
-        for(int i=0;i<=amount;i++)
-        {
-            if(i%coins[0]==0) dp[0][i]=1;
-        }
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<=amount;j++)
-            {
-                int take=0,ntake=dp[i-1][j];
-                if(j-coins[i]>=0) take=dp[i][j-coins[i]];
-                dp[i][j]=take+ntake;
-            }
-        }
-        return dp[n-1][amount];
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        return solve(amount,coins.size()-1,coins,dp);
     }
 };
