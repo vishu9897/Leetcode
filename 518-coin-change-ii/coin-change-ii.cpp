ddd48780141ce27d597cf93ef174dbc55bd94951
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-05-06 13:53:41 +0530

    Time: 16 ms (32.20%) | Memory: 19.9 MB (42.77%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
-    int solve(int amount,int index,vector<int>&coins,vector<vector<int>> &dp)
-    {
-        if(index<0) return amount==0 ? 1 : 0;
-        if(dp[index][amount]!=-1) return dp[index][amount];
-        int take=0,ntake= solve(amount,index-1,coins,dp);
-        if(coins[index] <= amount) take= solve(amount-coins[index],index,coins,dp);
-        return dp[index][amount] = take+ntake;
-    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
         for(int i=0;i<=amount;i++){
             dp[0][i]= i%coins[0] == 0 ? 1 : 0;
         }
@@ -25,7 +17,7 @@ public:
                 
             }
         }
+
         return dp[n-1][amount];
-        return solve(amount,coins.size()-1,coins,dp);
     }
 };
