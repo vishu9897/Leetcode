25d3972a0fa3aad897bea71798a6b228687103b3
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-05-06 13:24:26 +0530

    Time: 43 ms (16.19%) | Memory: 26 MB (7.97%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,38 +1,19 @@
 class Solution {
+private:
+	int solve(string &text1,string &text2,int index1,int index2,vector<vector<int>>&dp){
+        if(index1<0 || index2<0) return 0;
+        if(dp[index1][index2] != -1) return dp[index1][index2];
+		if(text1[index1]==text2[index2]) return dp[index1][index2] = 1+ solve(text1,text2,index1-1,index2-1,dp);
+		return dp[index1][index2] = max(solve(text1,text2,index1,index2-1,dp),solve(text1,text2,index1-1,index2,dp));
+}
+
+
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n1=text1.size(),n2=text2.size();
-        vector<vector<int>> dp(n1,vector<int>(n2,0));
-        for(int i=0;i<n1;i++)
-        {
-            if(text2[0]==text1[i]){
-                dp[i][0]=1;
-            }
-            if(i!=0 && dp[i-1][0]==1) dp[i][0]=1;
-        }
-        for(int i=0;i<n2;i++)
-        {
-            if(text1[0]==text2[i]){
-                dp[0][i]=1;
-            }
-            if(i!=0 && dp[0][i-1]==1) dp[0][i]=1;
-        }
-        // for(int i=0;i<n1;i++)
-        // {
-        //     for(int j=0;j<n2;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        for(int i=1;i<n1;i++)
-        {
-            for(int j=1;j<n2;j++)
-            {
-                if(text1[i]==text2[j]) dp[i][j]= 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[n1-1][n2-1];
+    	int n1=text1.size();
+	    int n2=text2.size();
+        vector<vector<int>> dp(n1,vector<int>(n2,-1));
+	return solve(text1,text2,n1-1,n2-1,dp);
     }
 };
+
