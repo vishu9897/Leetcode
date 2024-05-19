7e5a79dbd8f280f7e5915ee06334293a21834d39
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-05-19 17:22:48 +0530

    Time: 121 ms (72.26%) | Memory: 68.8 MB (98.94%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,39 +1,55 @@
 class Solution {
-public:
-    void heapify_down(vector<int>&nums,int i,int n)
+private:
+    int findParent(int i)
     {
-        int left= 2*i+1;
-        int right= 2*i+2;
+        return (i-1)/2;
+    }
+    int left(int i)
+    {
+        return 2*i+1;
+    }
+    int right(int i)
+    {
+        return i*2+2;
+    }
+    void heapify(vector<int>&nums,int i,int n)
+    {
         int s=i;
-
-        if(left<n && nums[s] < nums[left])
-        s=left;
-        if(right<n && nums[s] < nums[right])
-        s=right;
-
-        if(s!=i)
+        int l=left(i);
+        int r=right(i);
+        if(l<n && nums[s] < nums[l])
         {
-            swap(nums[i],nums[s]);
-            heapify_down(nums,s,n);
+            s=l;
+        }
+        if(r<n && nums[s]<nums[r])
+        {
+            s=r;
         }
 
+        if(i!=s)
+        {
+            swap(nums[s],nums[i]);
+            heapify(nums,s,n);
+        }
+        
     }
-    void build_heap(vector<int> &nums,int n)
+    void build_heap(vector<int> &nums)
     {
-        for(int i=(n/2)-1;i>=0;i--)
+
+        int n=nums.size();
+        for(int i=n/2-1;i>=0;i--)
         {
-            heapify_down(nums,i,n);
+            heapify(nums,i,n);
         }
     }
+public:
     vector<int> sortArray(vector<int>& nums) {
-        int n=nums.size();
-        build_heap(nums,n);
-        for(int i=n-1;i>=0;i--)
+        build_heap(nums);
+        for(int i=nums.size()-1;i>=0;i--)
         {
-            swap(nums[0],nums[i]);
-
-            heapify_down(nums,0,i);
-        }
+            swap(nums[i],nums[0]);
+            heapify(nums,0,i);
+        }   
         return nums;
     }
 };
