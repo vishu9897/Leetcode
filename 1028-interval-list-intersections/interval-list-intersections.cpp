26285d173905b08aa817485013fd1ea9aaf9c400
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-05-23 14:00:39 +0530

    Time: 19 ms (88.06%) | Memory: 22.3 MB (65.79%) - LeetSync

---
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>> vc;
+        pair<int,int> interval;
+        int i=0,j=0;
+        if(firstList.size()==0 || secondList.size()==0){
+            return vc;
+        }
+        
+        while(i<firstList.size() && j<secondList.size())
+        {
+            if(firstList[i][0] >= secondList[j][0] && firstList[i][0] <= secondList[j][1]) {
+                vc.push_back({max(firstList[i][0],secondList[j][0]),min(firstList[i][1],secondList[j][1])});
+            }
+            else if(secondList[j][0] >= firstList[i][0] && secondList[j][0] <= firstList[i][1]){
+                vc.push_back({max(firstList[i][0],secondList[j][0]),min(firstList[i][1],secondList[j][1])});
+            }
+            if(firstList[i][1] < secondList[j][1]) i++;
+            else j++;
+        }
+        
+        return vc;
+    }
+};
