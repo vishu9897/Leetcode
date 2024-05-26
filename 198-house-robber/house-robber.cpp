a050f474736e890e2d5cbddf8ba06ed83a900172
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-05-26 14:37:46 +0530

    Time: 4 ms (31.94%) | Memory: 9.1 MB (58.86%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
+        int prev=0;
+        int prev_prev=0;
+        int curr=0;
         vector<int> dp(n,-1);
         for(int i=0;i<n;i++)
         {
             int take=nums[i],ntake=0;
-            if(i-2 >= 0) take +=dp[i-2];
-            if(i-1 >= 0) ntake +=dp[i-1];
-            dp[i]=max(take,ntake);
+            if(i-2 >= 0) take += prev_prev;
+            if(i-1 >= 0) ntake += prev;
+            curr=max(take,ntake);
+            prev_prev=prev;
+            prev=curr;
         }
-        return dp[n-1];
+        return curr;
     }
 };
