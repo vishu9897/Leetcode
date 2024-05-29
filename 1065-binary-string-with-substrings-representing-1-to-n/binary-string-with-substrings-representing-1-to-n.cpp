7dc1175b7a8b4c2beeebf493b6f23d4fdb32e9e2
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-05-29 20:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (70.66%) - LeetSync

--- 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.cpp
+++ 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
-        bool queryString(string S, int N) {
-        for (int i = N; i > N / 2;  --i) {
-            string b = bitset<32>(i).to_string();
-            if (S.find(b.substr(b.find("1"))) == string::npos)
+	// function to return binary form of any number
+    string binary(int n){
+        string ans="";
+        while(n){
+            ans.push_back(n%2+'0');
+            n/=2;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+    bool queryString(string s, int n) {
+		//  only check for upper half of the range
+        for(int i=n; i>=n/2; i--){
+            string b = binary(i);
+            if(s.find(b)==string::npos){
                 return false;
+            }
         }
         return true;
     }
