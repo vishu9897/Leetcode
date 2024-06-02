e052dea5dba01fc402765fcda89ab7ed8463e963
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-06-02 13:52:54 +0530

    Time: 206 ms (35.66%) | Memory: 11.9 MB (79.81%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int i,int j,vector<vector<int>> &dp)
+    int solve(vector<int> &nums,int i,int j,vector<vector<int>> &dp)
     {
         if(i>=j) return 0;
-        int maxCost=INT_MIN;
         if(dp[i][j]!=-1) return dp[i][j];
+        int maxi=INT_MIN;
         for(int k=i;k<j;k++)
         {
-            int cost= (nums[i-1] * nums[k] * nums[j]) + solve(nums,i,k,dp) + solve(nums,k+1,j,dp);
-            maxCost= max(maxCost,cost);
+            int cases= nums[i-1]*nums[j]*nums[k] + solve(nums,i,k,dp) + solve(nums,k+1,j,dp);
+            maxi=max(maxi,cases);
         }
-        return dp[i][j]=maxCost;
+        return dp[i][j]=maxi;
     }
     int maxCoins(vector<int>& nums) {
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        int n=nums.size();
-        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
-        for(int i=n-1;i>=1;i--){
-            for(int j=1;j<n;j++)
-            {
-                int maxCost=INT_MIN;
-                if(i>=j) continue;
-                for(int k=i;k<j;k++)
-                {
-                    int cost= nums[i-1]*nums[k]*nums[j] + dp[i][k] + dp[k+1][j];
-                    maxCost= max(maxCost,cost);
-                }
-                dp[i][j]=maxCost;
-            }
-        }
-        return solve(nums,1,n-1,dp);
-        // return 0;
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),-1));
+        return solve(nums,1,nums.size()-1,dp);
     }
 };
