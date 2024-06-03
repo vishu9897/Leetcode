30e075bb85049a78e451c7ed9441644362ef1ca1
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-06-03 21:53:16 +0530

    Time: 1687 ms (41.67%) | Memory: 107 MB (50.00%) - LeetSync

---
+++ 3436-find-subarray-with-bitwise-and-closest-to-k/find-subarray-with-bitwise-and-closest-to-k.cpp
@@ -0,0 +1,67 @@
+class Solution {
+
+public:
+    void buildTree(vector<int> &seg,vector<int> &nums,int low,int high,int pos)
+    {
+        if(low==high)
+        {
+            seg[pos]=nums[low];
+            return;
+        }
+
+        int mid= low + (high-low)/2;
+
+        buildTree(seg,nums,low,mid,2*pos+1);
+        buildTree(seg,nums,mid+1,high,2*pos+2);
+
+        seg[pos]=seg[2*pos+1] & seg[2*pos+2];
+    }
+
+    int query(vector<int> &seg,int low,int high,int pos,int qlow,int qhigh)
+    {
+        if(low>=qlow && high<=qhigh) return seg[pos];
+        if(low > qhigh || high < qlow) return INT_MAX;
+
+        int mid = low + (high -low)/2;
+
+        int leftSeg=query(seg,low,mid,2*pos+1,qlow,qhigh);
+        int rightSeg=query(seg,mid+1,high,2*pos+2,qlow,qhigh);
+
+        return leftSeg & rightSeg;
+
+
+    }
+    int minimumDifference(vector<int>& nums, int k) {
+        int n=nums.size();
+        // int x=log(n)/log(2);
+        // int si= 2 * pow(2,x) - 1;
+        vector<int> seg(4*n,0);
+        int ans=INT_MAX;
+        buildTree(seg,nums,0,n-1,0);
+
+        for(int i=0;i<n;i++)
+        {
+            int low=i;
+            int high=n-1;
+            int ind=-1;
+
+            while(low<=high)
+            {
+                int mid= low + (high-low)/2;
+                if(query(seg,0,n-1,0,i,mid) >= k){
+                    low=mid+1;
+                    ind=mid;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+            if(ind!=-1){
+                ans = min(ans,abs(query(seg,0,n-1,0,i,ind)-k));
+            }
+                ans = min(ans,abs(query(seg,0,n-1,0,i,i)-k));
+                ans = min(ans,abs(query(seg,0,n-1,0,i,high+1)-k));
+        }
+    return ans;    
+    }
+};
