1972f709a3f4f6154897c30d0d9c3464c91c5bd6
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-06-03 22:11:04 +0530

    Time: 1637 ms (41.67%), Space: 103.2 MB (50%) - LeetHub

--- 3436-find-subarray-with-bitwise-and-closest-to-k/3436-find-subarray-with-bitwise-and-closest-to-k.cpp
+++ 3436-find-subarray-with-bitwise-and-closest-to-k/3436-find-subarray-with-bitwise-and-closest-to-k.cpp
@@ -33,9 +33,9 @@ public:
     }
     int minimumDifference(vector<int>& nums, int k) {
         int n=nums.size();
-        // int x=log(n)/log(2);
-        // int si= 2 * pow(2,x) - 1;
-        vector<int> seg(4*n,0);
+        int x=ceil(log(n)/log(2));
+        int si= 2 * pow(2,x) - 1;
+        vector<int> seg(si,0);
         int ans=INT_MAX;
         buildTree(seg,nums,0,n-1,0);
 
