754f64e149efa4624d0950339db8d21ef108a757
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-06-07 21:23:40 +0530

    Time: 2077 ms (5.03%), Space: 510.2 MB (7.03%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,26 +1,24 @@
-bool cmp(vector<int> &A,vector<int> &B)
+bool cmp(vector<int> A,vector<int> B)
 {
-    // if(A[0]==B[0])
-    //     return A[1]<B[1];
     return A[1] < B[1];
 }
 class Solution {
 public:
-    
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        
         sort(intervals.begin(),intervals.end(),cmp);
-        // for(auto x: intervals)
-        // {
-        //     cout<<x[0]<<" "<<x[1]<<endl;
-        // }
-        // start=intervals[0][0];
-        int end=intervals[0][1];
-        int res=0;
+        int ans=1;
+
+        int prev=intervals[0][1];
         for(int i=1;i<intervals.size();i++)
         {
-            if(end>intervals[i][0]) res++;
-            else end=intervals[i][1];
+            if(intervals[i][0]>=prev)
+            {
+                ans++;
+                prev=intervals[i][1];
+            }
         }
-        return res;
+
+        return intervals.size()-ans;
     }
 };
