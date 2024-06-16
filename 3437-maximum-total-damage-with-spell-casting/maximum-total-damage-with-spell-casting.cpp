cd19ea84560278d7889bed49fc1429d0ac9adeae
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-06-16 09:59:02 +0530

    Time: 532 ms (80.00%) | Memory: 214.1 MB (60.00%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-    long long solve(vector<pair<int,int>> &vc,int i,vector<long long> &dp)
-    {
-        if(i >= vc.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        long long take=0,ntake= solve(vc,i+1,dp);
-        
-            int k;
-            for(k=i;k<vc.size();k++)
-            {
-                long long temp= vc[i].first+2;
-                long long temp1=vc[k].first;
-                if(temp < temp1) break;
-            }
-            long long index= vc[i].second;
-            long long val= vc[i].first;
-            take = (long long) ((index * val) + solve(vc,k,dp));
-        return dp[i]=max(take , ntake);
-    }
     long long maximumTotalDamage(vector<int>& p) {
         map<int,int> mp;
         vector<pair<int,int>> vc;
@@ -29,27 +11,27 @@ public:
         {
            vc.push_back({x.first,x.second}); 
         }
-        vector<long long> dp(vc.size()+1,-1);
-    //     for(int i=vc.size()-1;i>=0;i--)
-    //     {
-    //         long long ntake=dp[i+1];
-    //         long long take=0;
-    //         long long j=i,k;
+        vector<long long> dp(vc.size()+1,0);
+        for(int i=vc.size()-1;i>=0;i--)
+        {
+            long long ntake=dp[i+1];
+            long long take=0;
+            long long j=i,k;
             
-    //         for(k=i;k<vc.size();k++)
-    //         {
-    //             long long temp= vc[i].first + 2;
-    //             long long temp1=vc[k].first;
-    //             if(temp < temp1) break;
-    //         }
-    //         long long index= vc[i].second;
-    //         long long val= vc[i].first;
-    //         take = (val * index) + dp[k];
+            for(k=i;k<vc.size();k++)
+            {
+                long long temp= vc[i].first + 2;
+                long long temp1=vc[k].first;
+                if(temp < temp1) break;
+            }
+
+            long long index= vc[i].second;
+            long long val= vc[i].first;
+            take = (val * index) + dp[k];
     
-    //         dp[i]=max(take,ntake);          
-    //     }
-    //    return dp[0];
-        return solve(vc,0,dp);
+            dp[i]=max(take,ntake);          
+        }
+        return dp[0];
         
     }
 };
