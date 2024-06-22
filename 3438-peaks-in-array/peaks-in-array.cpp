b22badd110e36104e08b1bf02d4994e6e6d8f7e2
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-06-22 13:39:21 +0530

    Time: 545 ms (80.56%) | Memory: 240.9 MB (54.49%) - LeetSync

--- 3438-peaks-in-array/peaks-in-array.cpp
+++ 3438-peaks-in-array/peaks-in-array.cpp
@@ -1,74 +1,99 @@
 class Solution {
-    int createST(int idx, int l, int r, vector<int> &peak, vector<int> &st) {
-        if(l>r) return 0;
-        if(l==r) st[idx] = peak[l];
-        else {
-            int m = l + (r-l)/2;
-            st[idx] = createST(2*idx+1, l, m, peak, st) + createST(2*idx+2, m+1, r, peak, st);
-        }
-        
-        return st[idx];
-    }
-    
-    void updateST(int idx, int l, int r, int &updateIdx, int diff, vector<int> &peak, vector<int> &st) {
-        if(l>r) return;
-        
-        if(l>updateIdx || r<updateIdx) return;
-        if(l<=updateIdx && r>=updateIdx) st[idx] += diff;
-        
-        if(l < r) {
-            int m = l + (r-l)/2;
-            updateST(2*idx+1, l, m, updateIdx, diff, peak, st);
-            updateST(2*idx+2, m+1, r, updateIdx, diff, peak, st);
-        }
-    }
-    
-    int queryST(int idx, int l, int r, int ql, int qr, vector<int> &peak, vector<int> &st) {
-        if(l>r) return 0;
-        if(ql > r || qr < l) return 0;
-        if(ql<=l && qr>=r) return st[idx];
-        
-        int m = l + (r-l)/2;
-        return queryST(2*idx+1, l, m, ql, qr, peak, st) + queryST(2*idx+2, m+1, r, ql, qr, peak, st);
-    }
-    
 public:
-    vector<int> countOfPeaks(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        vector<int> res, peak(n);
-        for(int i=1; i<n-1; i++) {
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) peak[i] = 1;
-        }
-        vector<int> st(4*n);
-        createST(0, 0, n-1, peak, st);
+    int createST(vector<int> &st,int ind,int l,int r,vector<int> &arr)
+    {
         
-        for(auto &q: queries) {
-            if(q[0] == 1) {
-                // using q[1]+1 and q[2]-1 because ignoring the ends of subarray as those should not be consideres in peak cound
-                int qst = queryST(0, 0, n-1, q[1]+1, q[2]-1, peak, st);
-                res.push_back(qst);
-            } else if(q[0] == 2) {
-                int idx = q[1], val = q[2];
-                nums[idx] = val;
-                vector<int> dir = {-1,0,1};
-                for(int k=0; k<3; k++) {
-                    int i = idx+dir[k];
-                    if(i-1>=0 && i+1<n) {
-                        int currPeak = peak[i];
-                        int nowPeak = (nums[i] > nums[i-1]) && (nums[i] > nums[i+1]);
-                        if(!currPeak && nowPeak) {
-                            // now it has become peak, so add 1 so that peak[i] = 1 
-                            updateST(0, 0, n-1, i, 1, peak, st);
+        if(l>r) return 0;
+        if(l==r) return st[ind] = arr[l];
+        int mid= l + (r-l)/2;
+        return st[ind] = createST(st,2*ind+1,l,mid,arr) + createST(st,2*ind+2,mid+1,r,arr);
+    }
+    int queryST(vector<int> &st,int ind,int l,int r, int ql,int qr,vector<int> &arr)
+    {
+        if(l>r) return 0;
+        if(l>qr || r<ql) return 0;
+        if(l>=ql && qr>=r) return st[ind];
+
+        int mid= l + (r-l)/2;
+        return queryST(st,2*ind+1,l,mid,ql,qr,arr) + queryST(st,2*ind+2,mid+1,r,ql,qr,arr);
+    }
+    // void updateST(vector<int> &st,int ind,int l,int r,int diff,int &updateInd,vector<int> &arr)
+    // {
+    //     if(l>r) return;
+    //     if(l> updateInd ||r< updateInd) return;
+    //     if(l<=updateInd && r>=updateInd) st[ind] += diff;
+    //     if(l<r)
+    //     {
+    //     int mid= l + (r-l)/2;
+    //     updateST(st,2*ind+1,l,mid,diff,updateInd,arr);
+    //     updateST(st,2*ind+2,mid+1,r,diff,updateInd,arr);
+    //     }
+    // }
+    int updateST(vector<int> &st,int ind,int l,int r,int diff,int &updateInd,vector<int> &arr)
+    {
+
+        if(l>r) return 0;
+        if(l> updateInd ||r< updateInd) return st[ind];
+        if(l==updateInd && r==updateInd){
+            return st[ind] = diff;
+        } 
+        if(l<r)
+        {
+            int mid= l + (r-l)/2;
+            st[ind]= updateST(st,2*ind+1,l,mid,diff,updateInd,arr) + updateST(st,2*ind+2,mid+1,r,diff,updateInd,arr);
+        }
+        return st[ind];
+    }
+
+    vector<int> countOfPeaks(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size();
+        vector<int> st(4*n , 0);
+        vector<int> peak(n,0);
+        vector<int> res;
+        for(int i=1;i<n-1;i++) 
+        {
+            peak[i]= (nums[i] > nums[i-1]  && nums[i] > nums[i+1])  ? 1 : 0;
+        }
+        createST(st,0,0,n-1,peak);
+        
+        for(auto &q: queries)
+        {
+            if(q[0]==1)
+            {
+                int left=q[1]+1,right=q[2]-1;
+                int ans=queryST(st,0,0,n-1,left,right,peak);
+                
+                res.push_back(ans);
+            }   
+            else
+            {
+                int ind=q[1], val=q[2];
+                nums[ind]=val;
+
+                int dir[3]={-1,0,1};
+                for(int i=0;i<3;i++)
+                {
+                    int currInd= ind+dir[i];
+
+                    if(currInd-1 >= 0 && currInd+1 < n)
+                    {
+                        int changedPeak= ((nums[currInd] > nums[currInd+1]) && (nums[currInd] > nums[currInd -1])) ? 1  : 0 ;  
+                        int currPeak = peak[currInd];
+
+                        if(currPeak && !changedPeak){
+                            updateST(st,0,0,n-1,0,currInd,peak);
                         } 
-                        if(currPeak && !nowPeak) {
-                            // now it is no longer peak, so dec 1 and make peak[i] = 0
-                            updateST(0, 0, n-1, i, -1, peak, st);
-                        }
-                        peak[i] = nowPeak;
+                        else if(!currPeak && changedPeak){
+                            updateST(st,0,0,n-1,1,currInd,peak);
+                        } 
+                        peak[currInd]=changedPeak;
+                        
                     }
                 }
             }
         }
+        
+        
         return res;
     }
 };
