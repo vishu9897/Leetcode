e72bfbe6510d9029808488a257dc0712cfb8f817
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-07-15 10:58:49 +0530

    Time: 59 ms (42.55%), Space: 33 MB (45.17%) - LeetHub

--- 0930-all-possible-full-binary-trees/0930-all-possible-full-binary-trees.cpp
+++ 0930-all-possible-full-binary-trees/0930-all-possible-full-binary-trees.cpp
@@ -15,12 +15,14 @@ public:
     vector<TreeNode*> generateFBT(int start,int end)
     {
         if(start>end) return {NULL};
-        if((end-start+1)%2==0) return{};
+        if((end-start+1)%2==0) {
+            return{};
+        }
         string Key = to_string(start)+"-"+to_string(end);
         if(dp.find(Key)!=dp.end()) return dp[Key];
         vector<TreeNode*> leftNodes,rightNodes;
         vector<TreeNode*> res;
-        // TreeNode* root= new TreeNode(0);
+       
         for(int i=start;i<=end;i++){
             leftNodes= generateFBT(start,i-1);
             rightNodes= generateFBT(i+1,end);
@@ -29,7 +31,8 @@ public:
                 for(auto r: rightNodes)
                 {
                     TreeNode* root= new TreeNode(0);
-                    
+                    // if(l && r)
+                    // cout<<l->val<<" "<<r->val<<endl;
                     root->left= l;
                     root->right= r;
                     res.push_back(root);
