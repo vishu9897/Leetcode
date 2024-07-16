2829adfcc72fac87d022d0a2dcd77d1384ff019a
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-07-16 22:27:15 +0530

    Time: 116 ms (50.87%), Space: 95.2 MB (20.97%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -5,48 +5,24 @@ public:
         {
             nums[i]=nums[i]%2;
         }
-        int res=INT_MIN,len=0;
+        int res=INT_MIN,len=0,len1=0,cnt=1,cnt1=0,len2=0,len3=0;
         //all zero
         for(auto x: nums)
         {
             if(x==0) len++;
-        }
-        res=max(res,len);
-        len=0;
-        
-        // all One
-        for(auto x: nums)
-        {
-            if(x==1) len++;
-        }
-        res=max(res,len);
-        len=0;
-
-        //zero one 
-        int cnt=0;
-        for(auto x: nums)
-        {
+            if(x==1) len1++;
             if(x==cnt)
             {
-                len++;
-                cnt = cnt==0 ? 1 : 0;
-            } 
-        }
-
-        res=max(res,len);
-        len=0;
-        cnt=1;
-        //one Zero
-         for(auto x: nums)
-        {
-            if(x==cnt)
+                cnt =  cnt ==0? 1: 0;
+                len2++;
+            }
+            if(x==cnt1)
             {
-                len++;
-                cnt = cnt==0 ? 1 : 0;
+                cnt1= cnt1==0?1:0;
+                len3++;
             } 
         }
-
-        res=max(res,len);
-        return res;
+        res=max(max(max(len,len1),len2),len3);
+                return res;
     }
 };
