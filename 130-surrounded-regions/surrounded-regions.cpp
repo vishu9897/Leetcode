c78f5520e0e8a519e9ae6f2af57e5afc9be4ea85
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-07-18 12:16:26 +0530

    Time: 7 ms (92.30%) | Memory: 14.1 MB (31.93%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int cl[4]={0,-1,1,0};
     int rw[4]={-1,0,0,1};
-    void solve(vector<vector<char>> &board,int row,int col,vector<vector<char>> &ans,vector<vector<int>> &vis)
+    void solve(vector<vector<char>> &board,int row,int col,vector<vector<char>> &ans)
     {
         int n=board.size();
         int m=board[0].size();
 
         ans[row][col]='O';
 
-        vis[row][col]=1;
-
         for(int k=0;k<4;k++)
         {
             int r = row + rw[k];
             int c = col + cl[k];
 
-            if(r>=0 && r < n && c>=0 && c < m && board[r][c]=='O' && vis[r][c]==0)
+            if(r>=0 && r < n && c>=0 && c < m && board[r][c]=='O' && ans[r][c]=='X')
             {
-                solve(board,r,c,ans,vis);
+                solve(board,r,c,ans);
             }
         }
     }
@@ -31,30 +29,26 @@ public:
 
         for(int i=0;i<n;i++)
         {
-            vector<vector<int>> vis(n,vector<int>(m,0));
             if(board[i][0]=='O'){
-                solve(board,i,0,ans,vis);
+                solve(board,i,0,ans);
             } 
         }
         for(int i=0;i<n;i++)
         {
-            vector<vector<int>> vis(n,vector<int>(m,0));
             if(board[i][m-1]=='O'){
-                solve(board,i,m-1,ans,vis);
+                solve(board,i,m-1,ans);
             }   
         }
         for(int j=0;j<m;j++)
         {
-            vector<vector<int>> vis(n,vector<int>(m,0));
             if(board[0][j]=='O'){
-                solve(board,0,j,ans,vis);
+                solve(board,0,j,ans);
             }
         }
         for(int j=0;j<m;j++)
         {
-            vector<vector<int>> vis(n,vector<int>(m,0));
             if(board[n-1][j]=='O'){
-                solve(board,n-1,j,ans,vis);
+                solve(board,n-1,j,ans);
             } 
         }
 
