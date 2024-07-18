791f1b0de0ce874596d37b1212008f3a8281bfb9
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-07-18 09:24:00 +0530

    Time: 54 ms (58.60%) | Memory: 38.3 MB (16.65%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,51 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size(),m=mat[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        int n=mat.size();
+        int m=mat[0].size();
         vector<vector<int>> vis(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>> q;
-        vector<vector<int>> ans(n,vector<int>(m,0));
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {   
-                if(mat[i][j]==0){
+        vector<vector<int>> res(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0)
+                {
                     vis[i][j]=1;
                     q.push({{i,j},0});
-                } 
-                
+                }
             }
         }
-        int rw[4]={-1,0,0,1};
-        int cl[4]={0,-1,1,0};
-        while(!q.empty())
-        {
-            pair<pair<int,int>,int> pr=q.front();
+        int row[4]= {-1,0,0,1};
+        int col[4]= {0,-1,1,0};
+
+        while(!q.empty()){
+            int rw= q.front().first.first;
+            int cl= q.front().first.second;
+            int step= q.front().second;
             q.pop();
 
-
-            int i=pr.first.first;
-            int j=pr.first.second;
-            int step=pr.second;
-            ans[i][j]=step;
+            res[rw][cl]=step;
 
             for(int k=0;k<4;k++)
             {
-                int row= rw[k] + i;
-                int col= cl[k] + j;
+                int fr= rw + row[k];
+                int fc= cl + col[k];
 
-                if(row>=0 && row<n && col>=0 && col<m){
-                    if(vis[row][col]==0 && mat[row][col]==1)
-                    {
-                        vis[row][col]=1;
-                        q.push({{row,col},step+1});
-                    }
-                }
-
+                if(fr>=0 && fc >=0 && fr<n && fc<m && mat[fr][fc]==1 && vis[fr][fc]==0)
+                {
+                    vis[fr][fc]=1;
+                    q.push({{fr,fc},step+1});
+                } 
             }
 
         }
-        return ans;
+
+        return res;
     }
 };
