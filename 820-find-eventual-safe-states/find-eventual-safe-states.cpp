b3955fa171b1a1b98193e55faede8894c1e0f53d
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-07-19 20:30:58 +0530

    Time: 102 ms (90.81%) | Memory: 52.1 MB (68.11%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> res;
-        vector<int> color(graph.size(),0);
-        for(int i=0;i<graph.size();i++)
+    bool solve(vector<vector<int>> &graph,int x,vector<int> &vis,vector<int> &pathVis,vector<int> &check)
+    {
+        vis[x]=1;
+        pathVis[x]=1;
+        check[x]=0;
+        for(auto ele: graph[x])
         {
-            if(dfs(graph,color,i)) res.push_back(i);
+            if(vis[ele]==0)
+            {
+                if(solve(graph,ele,vis,pathVis,check)==true) {
+                    // check[x]=0;
+                    return true;
+                }
+            }
+            else if(pathVis[ele]){
+                // check[x]=0;
+                return true;
+            } 
+        }
+        check[x]=1;
+        pathVis[x]=0;
+
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int m=graph.size();
+        vector<int> vis(m,0);
+        vector<int> pathVis(m,0);
+        vector<int> check(m,0);
+
+        vector<int> res;
+        for(int i=0;i<m;i++)
+        {
+            if(vis[i]==0)
+            {
+                solve(graph,i,vis,pathVis,check);
+            }
+        }
+        for(int i=0;i<check.size();i++)
+        {
+            if(check[i]==1) res.push_back(i);
         }
         return res;
-    }       
-    bool dfs(vector<vector<int>> &graph,vector<int> &color,int src)
-    {
-        if(color[src]>0)
-         return color[src]==2;
-        color[src]=1;
-        for(auto x:graph[src])
-        {
-            if(color[x]==1 || !dfs(graph,color,x)) return false;
-        }
-        color[src]=2;
-        return true;
     }
-
 };
