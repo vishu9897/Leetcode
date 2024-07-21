16695e179c880d9122a4056589e0fd75c373b759
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-07-21 08:14:09 +0530

    Time: 54 ms (95.98%) | Memory: 35.1 MB (93.76%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -28,37 +28,34 @@ public:
         {
             parent[i]=i;
         }
-        for(int i=0;i<accounts.size();i++)
+        int n=accounts.size();
+        for(int i=0;i<n;i++)
         {
-            for(int j=1;j<accounts[i].size();j++)
-            {
-                string s= accounts[i][j];
-                //find in map
+            for(int j=1;j<accounts[i].size();j++){
+                string s=accounts[i][j];
+
                 if(mp.find(s)!=mp.end())
                 {
-                    int u=mp[s];
-                    int v=i;
-
+                    int u= i;
+                    int v= mp[s];
                     unionThem(u,v,parent,rank);
                 }
                 else{
                     mp[s]=i;
                 }
-            }   
-        }
-        vector<vector<string>> vc(accounts.size());
-        set<string> st;
 
+            }
+        }
+        vector<vector<string>>  res;
+        vector<vector<string>> vc(accounts.size());
         for(auto x: mp)
         {
-            string s= x.first;
-            int node =x.second;
+            string s=x.first;
+            int node=x.second;  
 
-            node= findParent(node,parent);
-
+            node=findParent(node,parent);
             vc[node].push_back(s);
         }
-        vector<vector<string>> res;
         for(int i=0;i<vc.size();i++)
         {
             if(vc[i].size()>0){
@@ -69,8 +66,7 @@ public:
             }
         }
 
-
-
         return res;
+
     }
 };
