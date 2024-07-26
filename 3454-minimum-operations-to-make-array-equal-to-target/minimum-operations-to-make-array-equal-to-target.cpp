6fc887aed061cd69c25399d2b0c257b5d80d20fc
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-07-26 16:35:03 +0530

    Time: 114 ms (88.84%) | Memory: 122.2 MB (87.39%) - LeetSync

--- 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
+++ 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
@@ -1,52 +1,27 @@
 class Solution {
 public:
     long long minimumOperations(vector<int>& nums, vector<int>& target) {
-        long long res=0;
-        //incrementing first
-        int decre=0,dec=0,inc=0,inre=0,minInc=0,minDec=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            inc=target[i]-nums[i] >=0  ?  target[i]-nums[i] : 0;
-            inc= inc-minInc;
-            if(inre > inc)
-            {
-                res+= inre;
-                minInc+=inc;
-                inre=0;
+        int n = nums.size();
+        long incr = 0, decr = 0, ops = 0;
 
-            }
-            else{
-                inre=inc;
-            }
-            // cout<<i<<" "<<res<<" "<<inre<<endl;
-        }
-        // Dec
-        for(int i=0;i<nums.size();i++)
-        {
-            dec=target[i]-nums[i] >=0  ? 0 : nums[i]-target[i];
-            dec=dec-minDec;
-            if(decre > dec)
-            {
-                res+=decre;
-                minDec+=dec;
-                decre=0;
-            } 
-            else
-            {
-                decre=dec;
+        for (int i = 0; i < n; i++) {
+            long diff = target[i] - nums[i];
+
+            if (diff > 0) {
+                if (incr < diff)
+                    ops += diff - incr;
+                incr = diff;
+                decr = 0;
+            } else if (diff < 0) {
+                if (diff < decr)
+                    ops += decr - diff;
+                decr = diff;
+                incr = 0;
+            } else {
+                incr = decr = 0;
             }
         }
-        // cout<<inc<<" "<<inre<<" "<<dec<<" "<<decre<<endl;
-        if(inc!=0)
-        {
-            res+=inre;
-            inre=0;
-        }
-        if(dec!=0)
-            {
-                res+=decre;
-                decre=0;
-            } 
-        return res;
+
+        return ops;
     }
 };
