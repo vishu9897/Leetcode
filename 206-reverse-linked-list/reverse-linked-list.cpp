c290f20514d84e353f440913a99b803867425e3e
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-08-07 17:57:41 +0530

    Time: 4 ms (66.15%) | Memory: 13.1 MB (8.53%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,28 +10,15 @@
  */
 class Solution {
 public:
-    ListNode* solve(ListNode* node,ListNode* &resNode)
-    {
-        if(node==NULL || node->next==NULL ) return node;
-        ListNode* newNode=solve(node->next,resNode);
-        ListNode* front=node->next;
-        front->next=node;
-        node->next=NULL;
-        return newNode;
-            // ListNode* temp=node->next;
-            // node->next=NULL;
-            // temp->next=node;
-    }
+    
     ListNode* reverseList(ListNode* head) {
-        ListNode* resNode=NULL;
+
         if(head==NULL || head->next==NULL) return head;
-        return solve(head,resNode);
-        ListNode* temp= resNode;
-        while(temp!=NULL)
-        {
-            cout<<temp->val<<endl;
-            temp=temp->next;
-        }
-        return resNode;
+        ListNode* front=reverseList(head->next);
+        ListNode* temp=head->next;
+        temp->next=head;
+        head->next=NULL;
+        return front;
+
     }
 };
