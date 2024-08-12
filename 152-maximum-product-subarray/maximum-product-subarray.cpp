1a8238b85a42ecc87c510c80240252de2b5b2e3a
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-08-12 22:47:20 +0530

    Time: 3 ms (84.48%) | Memory: 16.5 MB (47.02%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int leftSubarray=1;
-        int rightSubarray=1;
-        int n=nums.size()-1,ans=INT_MIN;
-        for(int i=0;i<=n;i++)
+        long long mod=1e12+7;
+        long long suff=1,pref=1,ans=INT_MIN;
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++)
         {
-            leftSubarray *=  nums[i];
-            rightSubarray *= nums[n-i];
-            ans= max(ans,max(leftSubarray,rightSubarray));
-            if(nums[i]==0) leftSubarray=1;
-            if(nums[n-i]==0) rightSubarray=1;
-        }   
+            
+            
+            pref=(long long) (pref%mod *nums[i]%mod)%mod;
+            suff =(long long) (suff%mod*nums[n-i-1]%mod)%mod;
+            ans=max(ans,max(pref,suff));
+            if(nums[i]==0) pref=1;
+            if(nums[n-i-1]==0) suff=1;
+
+        }
         return ans;
     }
 };
