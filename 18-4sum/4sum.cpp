fcbf4fe757134b1229778e62caecd40beecec685
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-08-12 11:13:05 +0530

    Time: 20 ms (89.12%) | Memory: 16.2 MB (35.72%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> fourSum(vector<int>& arr, int target) {
+        int n=arr.size();
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        for(int i=0;i<n-3;i++)
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n;i++)
         {
-            if(i>0 && nums[i-1]==nums[i]) continue;
-
-            for(int j=i+1;j<n-2;j++)
-            {
-                if(j>i+1 && nums[j-1]==nums[j]) continue;
-
-                int k=j+1;
-                int l=n-1;
-
-                while(k<l)
+            if(i>0 && arr[i]==arr[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && arr[j]==arr[j-1]) continue;
+                int low=j+1,high=n-1;
+                while(low<high)
                 {
-                     
-                    long long sum= nums[i] + nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-                    if(sum<target)
-                    {
-                        k++;
-                    }
-                    else if(sum> target)
-                    {
-                        l--;
-                    }
-                    else
-                    {
-                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
-                        k++;
-                        l--;
-                        while(k<l && nums[k] == nums[k-1]) k++;
-                        while(k<l && nums[l] == nums[l+1]) l--;
-                    }
 
+                    long long sum =arr[i]+arr[j];
+                    sum +=arr[low];
+                    sum+= arr[high];
+                    if(sum==target)
+                    {
+                        ans.push_back({arr[i],arr[j],arr[low],arr[high]});
+                        low++;
+                        high--;
+                        while(low<high && arr[low]==arr[low-1]) low++;
+                        while(low<high && arr[high]==arr[high+1]) high--;
+                    }
+                    else if(sum>target){
+                        high--;
+                    }   
+                    else{
+                        low++;
+                    }
                 }
             }
-        }
+        }   
         return ans;
     }
 };
