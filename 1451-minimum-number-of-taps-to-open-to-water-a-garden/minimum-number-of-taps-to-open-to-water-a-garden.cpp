33b26e8eeb2c0af2b8121ad76e8b15c4c39c8158
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-08-14 15:22:16 +0530

    Time: 10 ms (67.73%) | Memory: 17.6 MB (47.15%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<int> arr(n + 1, 0);
-        for(int i = 0; i < ranges.size(); ++i) {
-            if(ranges[i] == 0) continue;
-            int left = max(0, i - ranges[i]);
-            arr[left] = max(arr[left], i + ranges[i]);
+        int ans;
+        vector<int> startEnd(n+1,0);
+        for(int i=0;i<ranges.size();i++)
+        {
+            int tapPoint= ranges[i];
+            int start= max(0,i-tapPoint);
+            int end= min(n,i+tapPoint);
+            startEnd[start]=max(startEnd[start],end);
         }
-        
-        int end = 0, far_can_reach = 0, cnt = 0;
-        for(int i = 0; i <= n; ++i) {
-            if(i > end) {
-                if(far_can_reach <= end) return -1;
-                end = far_can_reach;
-                ++cnt;
-            }
-            far_can_reach = max(far_can_reach, arr[i]);
+        int taps=0,currInd=0,maxInd=0;
+
+        // for(auto x:startEnd)
+        // {
+        //     cout<<x<<" ";
+        // }
+        // cout<<endl;
+        for(int i=0;i<=n;i++)
+        {
+            if(maxInd < i)return -1;
+            
+
+            if(currInd<i){
+                taps++;
+                currInd=maxInd;
+            } 
+            maxInd= max(startEnd[i],maxInd);
         }
-        
-        return cnt + (end < n);
+        return taps;
     }
 };
