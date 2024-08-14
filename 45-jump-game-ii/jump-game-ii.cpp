b79af13a8e8679af7fddd50032d0ff2ca665c52e
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-08-14 16:10:35 +0530

    Time: 12 ms (56.66%) | Memory: 19.1 MB (71.65%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        
-        int left=0,right=0;
-        int noOfJump=0,maxReach=0;
-        int n=nums.size(); 
-        for(int i=0;i<n;i++)
-        {
-            maxReach=max(maxReach,nums[i]+i);
-            if(i==right){
-                if(i==n-1) return noOfJump;
-                left=right+1;
-                right=maxReach;
-                noOfJump++;
-            }
+        int currInd=0,maxInd=0,steps=0;
+
+
+        for(int i=0;i<nums.size();i++){
+		if(currInd < i){
+			steps++;
+			currInd=maxInd;
+		}	
+		maxInd=max(maxInd,nums[i]+i);
         }
-        return noOfJump;
+        return steps;
     }
 };
