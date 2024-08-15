b20f36382caccb84c5f4e4857fac5da17e08ed9e
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-08-15 15:11:56 +0530

    Time: 88 ms (55.66%) | Memory: 69.6 MB (45.02%) - LeetSync

---
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -0,0 +1,22 @@
+// OJ: https://leetcode.com/problems/shortest-subarray-to-be-removed-to-make-array-sorted/
+// Author: github.com/lzl124631x
+// Time: O(N)
+// Space: O(1)
+// Ref: https://leetcode.com/problems/shortest-subarray-to-be-removed-to-make-array-sorted/discuss/830416/Java-Increasing-From-Left-Right-and-Merge-O(n)
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& A) {
+        int N = A.size(), left = 0, right = N - 1;
+        while (left + 1 < N && A[left] <= A[left + 1]) ++left;
+        if (left == A.size() - 1) return 0;
+        while (right > left && A[right - 1] <= A[right]) --right;
+        int ans = min(N - left - 1, right), i = 0, j = right;
+        while (i <= left && j < N) {
+            if (A[j] >= A[i]) {
+                ans = min(ans, j - i - 1);
+                ++i;
+            } else ++j;
+        }
+        return ans;
+    }
+};
