2a737aa8d9b231664ee6aae744d4470c96d03765
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-08-15 19:32:13 +0530

    Time: 3 ms (46.70%) | Memory: 7.7 MB (69.22%) - LeetSync

--- 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
-        string smallestSubsequence(string s) {
-        string res = "";
-        int last[26] = {}, seen[26] = {}, n = s.size();
-        for (int i = 0; i < n; ++i)
-            last[s[i] - 'a'] = i;
-        for (int i = 0; i < n; ++i) {
-            if (seen[s[i] - 'a']++) continue;
-            while (!res.empty() && res.back() > s[i] && i < last[res.back() - 'a'])
-                seen[res.back() - 'a'] = 0, res.pop_back();
-            res.push_back(s[i]);
+    string smallestSubsequence(string s) {
+        string ans="";
+        stack<char> st;
+        int last[26];
+        vector<int> seen(26,0);
+        for(int i=0;i<s.size();i++)
+        {
+            last[s[i]-'a']=i;
+        }   
+        // for(int i=0;i<26;i++)
+        // {
+        //     cout<<last[i]<<" ";
+        // }
+        // cout<<endl;
+        for(int i=0;i<s.size();i++)
+        {
+            while(!st.empty() && st.top() > s[i] && last[st.top()-'a'] > i && seen[s[i]-'a']==0){
+                seen[st.top()-'a']=0;
+                st.pop();
+                
+            }
+            if(seen[s[i]-'a'] == 0){
+                st.push(s[i]);
+                // cout<<i<<endl;
+            }
+            // st.push(s[i]);
+            seen[s[i]-'a']=1;
         }
-        return res;
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        // cout<<ans;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
