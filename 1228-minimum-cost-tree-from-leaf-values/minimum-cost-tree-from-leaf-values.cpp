de73578ecda8a90991f3c99e55025640ae7b4c1a
Vishal Gupta <47574357+vishu9897@users.noreply.github.com>
2024-08-15 13:26:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (89.40%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    pair<int,int> createTree(int low,int high,vector<int> &arr,vector<vector<pair<int,int>>> &dp){
-        if(low==high)
-        {
-            return {0,arr[low]};
+    int mctFromLeafValues(vector<int>& A) {
+        int res = 0;
+        vector<int> stack = {INT_MAX};
+        for (int a : A) {
+            while (stack.back() <= a) {
+                int mid = stack.back();
+                stack.pop_back();
+                res += mid * min(stack.back(), a);
+            }
+            stack.push_back(a);
         }
-        if(dp[low][high].first != -1) return dp[low][high];
-        int sum=0;
-        pair<int,int> ans;
-        ans={INT_MAX,INT_MIN};
-        for(int i=low;i<high;i++)
-        {
-            sum=0;
-            pair<int,int> left= createTree(low,i,arr,dp);
-            pair<int,int> right= createTree(i+1,high,arr,dp);
-            int val1=left.first;
-            int val2=right.first;
-            int ind1=left.second;
-            int ind2=right.second;
-            if(ans.first > val1+val2+(ind1*ind2))
-            ans= {val1+val2+(ind1*ind2),max(ind1,ind2)};            
+        for (int i = 2; i < stack.size(); ++i) {
+            res += stack[i] * stack[i - 1];
         }
-        return dp[low][high]=ans;
-    }
-    int mctFromLeafValues(vector<int>& arr) {
-        int n=arr.size();
-        vector<vector<pair<int,int>>> dp(n+1,vector<pair<int,int>>(n+1,{-1,-1}));
-        pair<int,int> solu=createTree(0,n-1,arr,dp);
-
-        return solu.first;
+        return res;
     }
 };
